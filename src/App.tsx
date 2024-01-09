36e64eada2920687b18854d16275e45c1b288f87
Renan Estery <renan.estery@compasso.com.br>
2024-01-09 11:22:39 -0300

    feature: add interfaces and extensions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Header } from './components/Header';
-import { Post } from './components/Post';
+import { Post, PostType } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 import styles from './App.module.css';
 
 import './global.css';
 
-const post = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -19,7 +19,7 @@ const post = [
       {
         type: 'paragraph',
         content:
-          ' Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
       },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
@@ -37,11 +37,11 @@ const post = [
       {
         type: 'paragraph',
         content:
-          ' Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
       },
       { type: 'link', content: 'jane.design/doctorcare' },
     ],
-    publishedAt: new Date('2023-05-03 20:00:00'),
+    publishedAt: new Date('2022-05-10 20:00:00'),
   },
 ];
 
@@ -53,15 +53,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          {post.map((post) => {
-            return (
-              <Post
-                key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
-              />
-            );
+          {posts.map((post) => {
+            return <Post key={post.id} post={post} />;
           })}
         </main>
       </div>
